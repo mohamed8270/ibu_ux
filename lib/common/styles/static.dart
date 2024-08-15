ec8ecce72fbe723c37f5d0a405076e1c809494dc
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-08-15 13:09:52 +0530

    static data [text and icons included]

---
+++ lib/common/styles/static.dart
@@ -0,0 +1,20 @@
+class StaticData {
+  // icons
+  static String leftarrow =
+      'https://www.svgrepo.com/show/510041/left-arrow.svg';
+  static String rightarrow =
+      'https://www.svgrepo.com/show/510165/right-arrow.svg';
+  static String uparrow = 'https://www.svgrepo.com/show/510301/up-arrow.svg';
+  static String instagram = 'https://www.svgrepo.com/show/520798/instagram.svg';
+  static String youtube = 'https://www.svgrepo.com/show/521051/youtube.svg';
+  static String linkedin =
+      'https://www.svgrepo.com/show/512419/linkedin-161.svg';
+  static String dribble = 'https://www.svgrepo.com/show/473590/dribble.svg';
+  static String github = 'https://www.svgrepo.com/show/501576/github.svg';
+
+  // text
+  static String hertxt =
+      'Relate to our creative works that are beyond your expectations';
+  static String herosub =
+      'Developer with solid design and development expertise';
+}
