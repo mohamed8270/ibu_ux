47386d4af0a63e7b8a87810989d7e52a7123c83f
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-08-25 21:07:05 +0530

    tap function updated in checkout section

--- lib/routes/pages/home_page/widgets/check_out/check_out_section.dart
+++ lib/routes/pages/home_page/widgets/check_out/check_out_section.dart
@@ -39,6 +39,7 @@ class CheckOutSection extends StatelessWidget {
                   title: out['name'].toString(),
                   content: out['des'].toString(),
                   pl: i == 0 ? 60.0 : 20.0,
+                  click: () {},
                 );
               },
             ),
@@ -55,12 +56,14 @@ class PlayfulContent extends StatelessWidget {
       required this.url,
       required this.title,
       required this.content,
-      required this.pl});
+      required this.pl,
+      required this.click});
 
   final String url;
   final String title;
   final String content;
   final double pl;
+  final VoidCallback click;
 
   @override
   Widget build(BuildContext context) {
@@ -68,31 +71,34 @@ class PlayfulContent extends StatelessWidget {
     var txt = TextFond();
     return Padding(
       padding: EdgeInsets.only(right: 20, left: pl),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: scrnsize.height * 0.25,
-            width: scrnsize.width * 0.25,
-            decoration: BoxDecoration(
-              color: iviolet,
-              image: DecorationImage(
-                image: NetworkImage(url),
-                fit: BoxFit.cover,
-                filterQuality: FilterQuality.high,
+      child: GestureDetector(
+        onTap: click,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: scrnsize.height * 0.25,
+              width: scrnsize.width * 0.25,
+              decoration: BoxDecoration(
+                color: iviolet,
+                image: DecorationImage(
+                  image: NetworkImage(url),
+                  fit: BoxFit.cover,
+                  filterQuality: FilterQuality.high,
+                ),
               ),
             ),
-          ),
-          StaticData.gap3,
-          txt.textWidget(title, 14.0, FontWeight.w600, iblack),
-          StaticData.gap1,
-          SizedBox(
-            height: scrnsize.height * 0.08,
-            width: scrnsize.width * 0.25,
-            child: txt.textWidget(content, 10.0, FontWeight.w400, iblack,
-                TextOverflow.visible, 1.5, TextAlign.justify),
-          ),
-        ],
+            StaticData.gap3,
+            txt.textWidget(title, 14.0, FontWeight.w600, iblack),
+            StaticData.gap1,
+            SizedBox(
+              height: scrnsize.height * 0.08,
+              width: scrnsize.width * 0.25,
+              child: txt.textWidget(content, 10.0, FontWeight.w400, iblack,
+                  TextOverflow.visible, 1.5, TextAlign.justify),
+            ),
+          ],
+        ),
       ),
     );
   }
