154e47eb8e6a3de2769145cc288a4c7277b29a9f
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-08-29 12:04:02 +0530

    flex updated in experience section

--- lib/routes/pages/home_page/widgets/experience_awards/experience_awards.dart
+++ lib/routes/pages/home_page/widgets/experience_awards/experience_awards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ibu_ux/bindings/controller/responsive_widget.dart';
 import 'package:ibu_ux/common/styles/theme.dart';
 import 'package:ibu_ux/routes/pages/home_page/widgets/experience_awards/reusable_widget.dart';
 
@@ -8,16 +9,25 @@ class ExperienceAwards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var scrnsize = MediaQuery.sizeOf(context);
+    double sizeForContainer() {
+      return ResponsiveWidget.isLarge(context) ? 0.6 : 1;
+    }
+
     return Container(
-      height: scrnsize.height * 0.6,
+      height: scrnsize.height * sizeForContainer(),
       width: scrnsize.width,
       decoration: const BoxDecoration(color: iyellow),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 40),
-        child: Row(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: ResponsiveWidget.isLarge(context) ? 60 : 20,
+            vertical: 40),
+        child: Flex(
+          direction: ResponsiveWidget.isLarge(context)
+              ? Axis.horizontal
+              : Axis.vertical,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+          children: const [
             ReusableWidget(),
             ReusableWidget2(),
           ],
